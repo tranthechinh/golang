f28effeb1876bad0fdc5f5349628effeb060b334
VinID <v.chinhtt1@vinid.net>
2021-08-30 14:52:19 +0700

    Golang lesson1

---
+++ hello-world/hello-world.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println ("Hello world")
+	Say ("Chào các bạn trong lớp Golang")
+	
+}
---
+++ hello-world/say.go
@@ -0,0 +1,5 @@
+package main
+import "fmt"
+func Say(msg string) {
+	fmt.Println (msg)
+}
