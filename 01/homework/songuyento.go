9835683bc1c67c65b0fe89984e013ffa95a65afe
VinID <v.chinhtt1@vinid.net>
2021-09-05 12:00:19 +0700

    số nguyên tố

--- 01/homework/songuyento.go
+++ 01/homework/songuyento.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -22,4 +23,31 @@ func main() {
 		return
 	}
 
+	if n < 2 {
+		fmt.Println("Số " + str + " không phải là số nguyên tố!")
+		return
+	}
+	fmt.Printf("Các số nguyên tố: ")
+	printPrimeNumbers(2, n)
+}
+
+func printPrimeNumbers(num1, num2 int) {
+	if num1 < 2 || num2 < 2 {
+		fmt.Println("Numbers must be greater than 2.")
+		return
+	}
+	for num1 <= num2 {
+		isPrime := true
+		for i := 2; i <= int(math.Sqrt(float64(num1))); i++ {
+			if num1%i == 0 {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			fmt.Printf("%d ", num1)
+		}
+		num1++
+	}
+	fmt.Println()
 }
